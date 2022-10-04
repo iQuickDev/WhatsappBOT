327ee1d3380518f1cf3f0277f1f37dd643ed05b6
iQuick <iquickpublic@gmail.com>
2022-10-04 17:17:19 +0200

    cpu temperature command + shuffled group list command

---
+++ commands/cputemp.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process')
+
+module.exports = {
+	name: 'cputemp',
+	description: 'display the temperature of the cpu',
+	category: 'Utility',
+	execute: (message, info) => {
+		exec(
+			'cat /sys/class/thermal/thermal_zone0/temp',
+			(error, stdout, stderr) => {
+                if (!isNaN(parseInt(stdout)))
+				message.reply(`CPU Temp: ${(parseInt(stdout) / 1000).toFixed(1)} °C`)
+                else
+                message.reply(`Can't retrieve the CPU temperature on device`)
+			}
+		)
+	},
+}
---
+++ commands/randomlist.js
@@ -0,0 +1,41 @@
+const index = require('../index.js')
+
+module.exports = {
+	name: 'randomlist',
+	description:
+		'generate a random ordered list of the participants of the group',
+	category: 'Utility',
+	execute: async (message, info) => {
+		let chat = await message.getChat()
+		let mentions = []
+		let users = []
+
+		if (chat.isGroup) {
+			let participants = chat.participants
+			let mentionString = '*SHUFFLED LIST*\n'
+
+			for (const participant of participants) {
+				let contact = await index.client.getContactById(
+					participant.id._serialized
+				)
+				mentions.push(contact)
+				users.push(participant.id.user)
+			}
+
+            shuffle(users)
+
+			for (let i = 0; i < users.length; i++) {
+				mentionString += `${i + 1}. @${users[i]}\n`
+			}
+
+			chat.sendMessage(mentionString, { mentions })
+		} else message.reply('This command can only be used in a group')
+	},
+}
+
+function shuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[array[i], array[j]] = [array[j], array[i]]
+	}
+}
