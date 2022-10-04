ac1a5ad3f7e9296bc28fb499f7145a76f9d69579
iQuick <iquickpublic@gmail.com>
2022-10-04 20:17:07 +0200

    cputemp and waifu command hotfix

--- commands/cputemp.js
+++ commands/cputemp.js
@@ -1,18 +1,12 @@
 const { exec } = require('child_process')
 
-module.exports = {
-	name: 'cputemp',
-	description: 'display the temperature of the cpu',
-	category: 'Utility',
-	execute: (message, info) => {
-		exec(
-			'cat /sys/class/thermal/thermal_zone0/temp',
-			(error, stdout, stderr) => {
-                if (!isNaN(parseInt(stdout)))
-				message.reply(`CPU Temp: ${(parseInt(stdout) / 1000).toFixed(1)} °C`)
-                else
-                message.reply(`Can't retrieve the CPU temperature on device`)
-			}
-		)
-	},
+export const name = 'cputemp'
+export const description = 'display the temperature of the cpu'
+export const category = 'Utility'
+export function execute(message, info) {
+	exec('cat /sys/class/thermal/thermal_zone0/temp', (error, stdout, stderr) => {
+		if (!isNaN(parseInt(stdout)))
+			message.reply(`CPU Temp: ${(parseInt(stdout) / 1000).toFixed(1)} °C`)
+		else message.reply(`Can't retrieve the CPU temperature on device`)
+	})
 }
